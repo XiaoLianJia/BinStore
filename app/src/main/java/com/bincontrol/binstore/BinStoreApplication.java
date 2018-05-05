f2d36f51fc5673d20e33bdfbcd8650b4407dcef4
zhangbin <m_zhangbin@foxmail.com>
2018-05-05 22:42:35 +0800

    首次提交

--- app/src/main/java/com/bincontrol/binstore/BinStoreApplication.java
+++ app/src/main/java/com/bincontrol/binstore/BinStoreApplication.java
@@ -5,17 +5,26 @@ import android.widget.Toast;
 
 import com.alibaba.baichuan.android.trade.AlibcTradeSDK;
 import com.alibaba.baichuan.android.trade.callback.AlibcTradeInitCallback;
+import com.alibaba.baichuan.android.trade.model.AlibcShowParams;
+import com.alibaba.baichuan.android.trade.model.OpenType;
 import com.alibaba.baichuan.trade.biz.AlibcTradeBiz;
 import com.alibaba.baichuan.trade.biz.core.taoke.AlibcTaokeParams;
 import com.alibaba.baichuan.trade.common.AlibcTradeCommon;
 
 import java.util.HashMap;
 
+import static com.bincontrol.binstore.common.AppConstant.ALI_CLIENT_ADZONE_ID;
+import static com.bincontrol.binstore.common.AppConstant.ALI_CLIENT_APP_KEY;
+import static com.bincontrol.binstore.common.AppConstant.ALI_CLIENT_PID;
+import static com.bincontrol.binstore.common.AppConstant.ALI_CLIENT_SUB_PID;
+
 
 public class BinStoreApplication extends Application {
 
     public static BinStoreApplication application = null;
     public static AlibcTaokeParams alibcTaokeParams = null;
+    public static AlibcShowParams alibcShowParams = null;
+
 
     @Override
     public void onCreate() {
@@ -26,8 +35,9 @@ public class BinStoreApplication extends Application {
         AlibcTradeBiz.turnOnDebug();
 
         // 初始化淘客参数
-        initTaokeParams();
-
+        initAlibcTaokeParams();
+        // 初始化商品展示参数
+        initAlibcShowParams();
         // 电商SDK初始化
         AlibcTradeSDK.asyncInit(this, new AlibcTradeInitCallback() {
             @Override
@@ -42,19 +52,31 @@ public class BinStoreApplication extends Application {
                 Toast.makeText(BinStoreApplication.this,  errorText, Toast.LENGTH_SHORT).show();
             }
         });
+
     }
+
 
     /**
      * 初始化淘客参数
      */
-    private void initTaokeParams() {
+    private void initAlibcTaokeParams() {
         alibcTaokeParams = new AlibcTaokeParams();
-        alibcTaokeParams.adzoneid = "428110369";
-        alibcTaokeParams.pid = "mm_128206999_44178876_428110369";
-        alibcTaokeParams.subPid = "mm_128206999_44178876_428110369";
+        alibcTaokeParams.adzoneid = ALI_CLIENT_ADZONE_ID;
+        alibcTaokeParams.pid = ALI_CLIENT_PID;
+        alibcTaokeParams.subPid = ALI_CLIENT_SUB_PID;
         alibcTaokeParams.extraParams = new HashMap<>();
-        alibcTaokeParams.extraParams.put("taokeAppkey", "24847671");
+        alibcTaokeParams.extraParams.put("taokeAppkey", ALI_CLIENT_APP_KEY);
     }
+
+
+    /**
+     * 初始化商品展示参数
+     */
+    private void initAlibcShowParams() {
+        alibcShowParams = new AlibcShowParams(OpenType.Native, false);
+        alibcShowParams.setClientType("taobao_scheme");
+    }
+
 
     public static BinStoreApplication getInstance() {
         if (application == null) {
